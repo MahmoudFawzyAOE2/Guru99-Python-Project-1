d47b7cc4208d132a1b45723527d97039ddb51adf
Mahmoud Fawzy Taha-Elaraby <76012086+MahmoudFawzyAOE2@users.noreply.github.com>
2024-03-17 12:39:18 +0200

    End of day1
    Reading the .csv file, transform each row to a dictionary

--- main.py
+++ main.py
@@ -4,4 +4,23 @@ Created on Sun Mar 17 10:20:55 2024
 
 @author: HP
 """
+import csv
 
+
+def main(): 
+    f = open("Emissions.csv", "r")
+    data = csv.reader(f)
+    for row in data:
+        row_d = dict([(row[0],row[1:])])
+        print(*[str(k) + ':' + str(v) for k,v in row_d.items()])
+
+
+
+
+
+
+
+
+if __name__== "__main__":
+    main()
+    
