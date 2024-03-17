abe35058ac357beab3294e4517e53860ffad945d
Mahmoud Fawzy Taha-Elaraby <76012086+MahmoudFawzyAOE2@users.noreply.github.com>
2024-03-17 17:19:55 +0200

    end of day 2
    modifications to code of day one, added  Guru answer of day1
    
    added some text to read.me file

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # Guru99-Python-Project-1
+Live Python Project: Data Analysis Project
+https://www.guru99.com/live-python-project.html
+
+Project Summary
+This project will put you in a corporate development environment. You will be coding Python application to perform Data Analysis on CO2 Emission for different countries. You will create & execute the code and have an opportunity to compare it Python code designed by our experts in real-time.
+
+day1: 
+    Read CSV File and store data in the dictionary
+
+day2 task: 
+    1- Take input from the user 
+    2- Calculate worldwide statistics (min, max, average) for a user-entered year
+    
+day3 task:
+
+
+day4 task:
+
+
+day5 task:
+
+
+day6 task:
+
+
+day7 task:
+
--- main.py
+++ main.py
@@ -6,13 +6,56 @@ Created on Sun Mar 17 10:20:55 2024
 """
 import csv
 
-
 def main(): 
+    
+    # reading the file
     f = open("Emissions.csv", "r")
     data = csv.reader(f)
+    
+    # transforming the rows into dictionaries & storing them in a list
+    rows_d = {}
     for row in data:
         row_d = dict([(row[0],row[1:])])
-        print(*[str(k) + ':' + str(v) for k,v in row_d.items()])
+        #print(*[str(k) + ':' + str(v) for k,v in row_d.items()])
+        
+        rows_d.update(row_d)
+        #print(rows_d)
+    # Taking input from the user
+    # Note: input shall not be changed to int as it's a string in the dictionary
+    year = input("Select a year to find statistics (1997 to 2010): ")
+    print(year)
+    
+    # Extracting index of the year
+    index = rows_d.get('CO2 per capita').index(year)
+    print(index)
+    
+    # Creating the list of emission in year
+    emission_list = []
+    countries = []
+    for key,value in rows_d.items():
+        emission_list.append(float(value[index]))
+        countries.append(key)
+     
+    emission_list.pop(0)
+    countries.pop(0)
+
+    print(emission_list)
+    
+    # Performing the analysis
+    Max_num = max(emission_list)
+    Max_index = emission_list.index(Max_num)
+    Max_country = countries[Max_index]
+    
+    Min_num = min(emission_list)
+    Min_index = emission_list.index(Min_num)
+    Min_country = countries[Min_index]
+    
+    Average_num = sum(emission_list) / len(emission_list)
+    
+    print("In {}, countries with minimum and maximum CO2 emission levels were: [{}] and [{}] respectively. Average CO2 emissions in {} were {:.6f}".format(year, Min_country, Max_country, year, Average_num))
+    
+    # Printing the data in required format using formatted string
+    
 
 
 
